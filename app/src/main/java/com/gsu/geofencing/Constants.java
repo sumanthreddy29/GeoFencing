21e833e1bfab494a4f871efcf8a89eb214052a69
minhaj <shujauddin.m9@gmail.com>
2019-04-24 23:21:00 -0400

    Building Push notification

--- app/src/main/java/com/gsu/geofencing/Constants.java
+++ app/src/main/java/com/gsu/geofencing/Constants.java
@@ -2,7 +2,7 @@ package com.gsu.geofencing;
 
 public class Constants {
 
-    public static final String CHANNEL_ID = "my_channel_";
-    public static final String CHANNEL_NAME = "my_channel_";
-    public static final String CHANNEL_DESCRIPTIONS = "my_channel_";
+    public static final String CHANNEL_ID = "Channel_01";
+    public static final String CHANNEL_NAME = "Notifications";
+    public static final String CHANNEL_DESCRIPTION = "Interests";
 }
--- app/src/main/java/com/gsu/geofencing/MainActivity.java
+++ app/src/main/java/com/gsu/geofencing/MainActivity.java
@@ -1,6 +1,12 @@
 package com.gsu.geofencing;
 
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.util.Log;
 import android.content.Intent;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,18 +29,72 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
-
+    Spinner spinner;
     LocationManager locationManager;
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, importance);
+            mChannel.setDescription(Constants.CHANNEL_DESCRIPTION);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+
+            MyNotificationManager.getInstance(this).displayNotification("Event Nearby", "Event based on your interest");
+
+        }
+
+        spinner = findViewById(R.id.spinnerTopics);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, importance);
+            mChannel.setDescription(Constants.CHANNEL_DESCRIPTION);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        findViewById(R.id.buttonSubscribe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String topic = spinner.getSelectedItem().toString();
+                FirebaseMessaging.getInstance().subscribeToTopic(topic);
+                Toast.makeText(getApplicationContext(), "Interest Subscribed", Toast.LENGTH_LONG).show();
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -55,6 +115,7 @@ public class MainActivity extends AppCompatActivity
              email = intent.getStringExtra("email");
 
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -190,4 +251,7 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+
 }
